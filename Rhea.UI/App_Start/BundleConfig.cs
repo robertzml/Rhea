2f15681ee1b7326fc3abbdb9422d4b467c4905f0
Robert <robertzml@126.com>
2013-07-04 16:25:48 +0800

    Setup the fluid layout with bootstrap.
    Signed-off-by: Robert <robertzml@126.com>

--- Rhea.UI/App_Start/BundleConfig.cs
+++ Rhea.UI/App_Start/BundleConfig.cs
@@ -18,6 +18,9 @@ namespace Rhea.UI
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                       "~/Scripts/bootstrap.js"));
+
             // 使用 Modernizr 的开发版本进行开发和了解信息。然后，当你做好
             // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
