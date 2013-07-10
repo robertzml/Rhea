4a3904f40aeebb8be11447cc2ca97e61cce81a64
Robert <robertzml@126.com>
2013-07-10 08:21:17 +0800

    Add department controller. Try to edit the svg room. This version will publish to the local server.
    Signed-off-by: Robert <robertzml@126.com>

---
+++ Rhea.UI/Controllers/DepartmentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Rhea.Business.Personnel;
+using Rhea.Model.Personnel;
+
+namespace Rhea.UI.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        #region Field
+        /// <summary>
+        /// 部门业务
+        /// </summary>
+        private IDepartmentService departmentService;
+        #endregion //Field
+
+        #region Function
+        protected override void Initialize(RequestContext requestContext)
+        {
+            if (departmentService == null)
+            {
+                departmentService = new MongoDepartmentService();
+            }
+
+            base.Initialize(requestContext);
+        }
+        #endregion //Function
+
+        #region Action
+        public ActionResult Index(int id)
+        {
+            return View(id);
+        }
+        #endregion //Action
+    }
+}
