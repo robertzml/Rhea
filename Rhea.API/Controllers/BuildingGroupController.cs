541654f5111e1fe020a0d3fef5fe58a287680136
Robert <robertzml@126.com>
2013-09-23 16:24:17 +0800

    add rhea.api project. add BuildingGroupController.cs. add get all building group.
    Signed-off-by: Robert <robertzml@126.com>

---
+++ Rhea.API/Controllers/BuildingGroupController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Rhea.Business.Estate;
+using Rhea.Model.Estate;
+
+namespace Rhea.API.Controllers
+{
+    public class BuildingGroupController : ApiController
+    {
+        #region Action
+        /// <summary>
+        /// 显示所有楼群
+        /// </summary>
+        /// <returns></returns>        
+        public HttpResponseMessage GetAll()
+        {
+            IBuildingGroupBusiness buildingGroupBusiness = new MongoBuildingGroupBusiness();
+            var data = buildingGroupBusiness.GetList();
+            
+            HttpResponseMessage response = Request.CreateResponse<List<BuildingGroup>>(HttpStatusCode.OK, data);
+            return response;
+            
+        }
+        #endregion //Action
+    }
+}
---
+++ Rhea.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rhea.API.WebApiApplication" Language="C#" %>
