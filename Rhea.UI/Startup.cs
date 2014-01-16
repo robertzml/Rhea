a97286c75a213878765841bb01d25ff12374b0c5
Robert <robertzml@126.com>
2014-01-16 14:12:44 +0800

    Add Rhea.UI project.
    Signed-off-by: Robert <robertzml@126.com>

---
+++ Rhea.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rhea.UI.Startup))]
+namespace Rhea.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
