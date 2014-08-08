26d392c385c9a243e7f5c009b6287b018eacc5ca
Robert Office <robertzml@126.com>
2014-08-08 14:13:05 +0800

    Add estate area.
    Signed-off-by: Robert Office <robertzml@126.com>

---
+++ Rhea.UI/Areas/Estate/Controllers/BuildingController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rhea.Business.Estate;
+using Rhea.Model.Estate;
+
+namespace Rhea.UI.Areas.Estate.Controllers
+{
+    /// <summary>
+    /// 建筑控制器
+    /// </summary>
+    public class BuildingController : Controller
+    {
+        #region Action
+        /// <summary>
+        /// 建筑主页
+        /// </summary>
+        /// <param name="id">建筑ID</param>
+        /// <returns></returns>
+        public ActionResult Index(int id)
+        {
+            BuildingBusiness business = new BuildingBusiness();
+            var data = business.Get(id);
+            return View(data);
+        }
+        #endregion //Action
+    }
+}
---
+++ Rhea.UI/Areas/Estate/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rhea.Business.Estate;
+using Rhea.Model.Estate;
+
+namespace Rhea.UI.Areas.Estate.Controllers
+{
+    /// <summary>
+    /// 房产管理主控制器
+    /// </summary>
+    public class HomeController : Controller
+    {
+        #region Action
+        /// <summary>
+        /// 房产管理主页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 建筑菜单
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult BuildingMenu()
+        {
+            BuildingBusiness business = new BuildingBusiness();
+            var data = business.Get().OrderBy(r => r.Sort);
+            return View(data);
+        }
+        #endregion //Action
+    }
+}
