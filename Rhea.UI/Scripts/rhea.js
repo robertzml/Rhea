505130a465bc400dd10ecbe046a2328afa11d0d1
robertzml <robertzml@126.com>
2014-11-24 15:24:04 +0800

    update plugin datatables to v1.10.4. fix the table filter bug.
    Signed-off-by: robertzml <robertzml@126.com>

--- Rhea.UI/Scripts/rhea.js
+++ Rhea.UI/Scripts/rhea.js
@@ -69,15 +69,19 @@ var Rhea = function () {
 					var select = $('<select class="form-control"><option value=""></option></select>')
 						.appendTo($(this).empty())
 						.on('change', function () {
-							var val = $(this).val();
+							//var val = $(this).val();
+							var val = $.fn.dataTable.util.escapeRegex(
+								$(this).val()
+							);
 							
-							oTable.api().column(i).search(val ? $(this).val() : val).draw();
-								//.search(val ? '^' + $(this).val() + '$' : val, true, false)
-								//.draw();
+							oTable.api().column(i)//.search(val ? $(this).val() : val).draw();
+								.search(val ? '^' + $(this).val() + '$' : val, true, false)
+								.draw();
 						});
 
 					oTable.api().column(i).data().unique().sort().each(function (d, j) {
-						if ($(d).html()) {
+						var re = /<[^>]+>/;
+						if (re.test(d)) {
 							select.append('<option value="' + $(d).html() + '">' + $(d).html() + '</option>')
 						} else {
 							select.append('<option value="' + d + '">' + d + '</option>')
@@ -157,6 +161,74 @@ var Rhea = function () {
 		});	
 		
 	}
+	
+	/* new init with filter */
+	var handleInitDatatable3 = function($dom) {
+		var oTable = $dom.DataTable({
+			"order": [],
+
+			"lengthMenu": [
+				[5, 10, 20, -1],
+				[5, 10, 20, "All"] // change per page values here
+			],
+			// set the initial value
+			"pageLength": 10,
+
+			"pagingType": "bootstrap_full_number",
+
+			"language": {
+					"lengthMenu": "  _MENU_ 记录",
+					"sLengthMenu": "每页 _MENU_ 条记录",
+					"sInfo": "显示 _START_ 至 _END_ 共有 _TOTAL_ 条记录",
+					"sInfoEmpty": "记录为空",
+					"sInfoFiltered": " - 从 _MAX_ 条记录中",
+					"sZeroRecords": "结果为空",
+					"sSearch": "搜索:",
+					"paginate": {
+						"previous":"Prev",
+						"next": "Next",
+						"last": "Last",
+						"first": "First"
+					}
+				},
+
+			"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12' p>>", // horizobtal scrollable datatable
+			
+			initComplete: function () {
+				var api = this.api();
+	 
+				api.columns().indexes().flatten().each( function ( i ) {
+					var column = api.column( i );
+					if ($(column.footer()).attr('data-filter') == 'true') {
+					
+						var select = $('<select class="form-control"><option value=""></option></select>')
+							.appendTo( $(column.footer()).empty() )
+							.on( 'change', function () {
+								var val = $.fn.dataTable.util.escapeRegex(
+									$(this).val()
+								);
+		 
+								column
+									.search( val ? '^'+val+'$' : '', true, false )
+									.draw();
+							});
+	 
+						column.data().unique().sort().each( function ( d, j ) {
+							var re = /<[^>]+>/;
+							if (re.test(d)) {
+								select.append('<option value="' + $(d).html() + '">' + $(d).html() + '</option>')
+							} else {
+								select.append('<option value="' + d + '">' + d + '</option>')
+							}
+						});
+					}
+				} );
+			}
+		});
+		
+		return oTable;
+	}
+	
 	
 	var handleInitDatePicker = function($dom, today) {
 		if (today == true) {
@@ -390,6 +462,10 @@ var Rhea = function () {
 			return handleInitDatatable1($dom, true);
 		},
 
+		initDatatableWithFilter2: function($dom) {
+			return handleInitDatatable3($dom);
+		},
+		
 		initDatePicker: function($dom, today) {
 			handleInitDatePicker($dom, today);
 		},
